d3f663c80bd0ce21ec6872fcbbe32baecc77ef58
Jalen Pinder <jtp1326@gmail.com>
2024-06-18 00:10:56 -0300

    Add app.use for bodyParser

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 const API_URL = "https://api.lyrics.ovh/v1/";
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try {
