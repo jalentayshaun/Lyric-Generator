f4a51b9f114efc914f7b46cdc97bada79780d12e
Jalen Pinder <jtp1326@gmail.com>
2024-06-18 00:08:38 -0300

    Add basic structure of get requests to server and Lyrics API

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 const API_URL = "https://api.lyrics.ovh/v1/";
 
 app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+        const artistName = req.body.artist;
+        const songName = req.body.song;
+        const result = await axios.get(
+            API_URL + 
+            artistName + "/" + 
+            songName
+        );
+        res.render("index.ejs", { lyrics: JSON.stringify(result.data) });
+    } catch (error) {
+        res.render("index.ejs", { lyrics: JSON.stringify(error.response.data) });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
